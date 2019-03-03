ad97e329620d16c2df318a4f006d2e2bf2d278ca
Daniel Abrecht <public@danielabrecht.ch>
2019-03-03 00:39:12 +0000

    Properly wait for any one child process to exit

--- makefile
+++ makefile
@@ -6,10 +6,11 @@ AR = ar
 
 PREFIX = /usr
 
-OPTIONS  = -ffunction-sections -fdata-sections -g -Og
-CC_OPTS  = -fvisibility=hidden -I include
-CC_OPTS += -std=c99 -Wall -Wextra -pedantic -Werror -D_DEFAULT_SOURCE
-LD_OPTS  = -Wl,-gc-sections -lutil
+OPTIONS += -ffunction-sections -fdata-sections -g -Og
+CC_OPTS += -fvisibility=hidden -I include
+CC_OPTS += -std=c99 -Wall -Wextra -pedantic -Werror
+CC_OPTS += -D_DEFAULT_SOURCE
+LD_OPTS += -Wl,-gc-sections
 
 LD_OPTS += -lttymultiplex
 
@@ -25,8 +26,9 @@ all: bin/console-keyboard-multiplexer
 build/%.o: src/%.c | build/.dir
 	$(CC) $(CC_OPTS) -c -o $@ $^
 
-bin/console-keyboard-multiplexer: build/console-keyboard-multiplexer.o bin/.dir
-	$(CC) $(LD_OPTS) -Wl,--whole-archive $< -Wl,--no-whole-archive -o $@
+bin/console-keyboard-multiplexer: build/console-keyboard-multiplexer.o
+	mkdir -p bin
+	$(CC) $(LD_OPTS) $^ -o $@
 
 install:
 	mkdir -p "$(DESTDIR)$(PREFIX)/bin/"
--- src/console-keyboard-multiplexer.c
+++ src/console-keyboard-multiplexer.c
@@ -1,12 +1,14 @@
 // Copyright (c) 2018 Daniel Abrecht
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <signal.h>
+#include <poll.h>
 #include <libttymultiplex.h>
 
 int keyboard_size;
@@ -49,6 +51,10 @@ void set_keyboard_size(int size){
   }
 }
 
+void cleanup(void){
+  tym_shutdown();
+}
+
 int forkcall(void* ptr, int(*init)(void*ptr), char* args[], int cfd){
   int ret = fork();
   if(ret == -1)
@@ -77,7 +83,18 @@ int execpane(int pane, char* argv[], int cfd){
   return forkcall(&pane, execpane_init, argv, cfd);
 }
 
+int childexitnotifier = -1;
+
+void childexit(int x){
+  (void)x;
+  write(childexitnotifier,"",1);
+}
+
 int main(){
+
+  atexit(cleanup);
+
+  // Initialise terminal multiplexer & add panes
   if(tym_init()){
     perror("tym_init failed");
     return 1;
@@ -95,18 +112,69 @@ int main(){
   }
   tym_pane_set_flag(bottom_pane, TYM_PF_DISALLOW_FOCUS, true);
   tym_pane_set_flag(top_pane, TYM_PF_FOCUS, true);
+
+  int sfd[2];
+  if(pipe(sfd) == -1){
+    perror("pipe failed");
+    return 1;
+  }
+  childexitnotifier = sfd[1];
+  signal(SIGCHLD, childexit);
+
+  // Execute programs;
   int tpid = execpane(   top_pane, (char*[]){"login", 0}, -1);
   int cfd[2];
-  pipe(cfd);
+  if(pipe(cfd) == -1){
+    perror("pipe failed");
+    return 1;
+  }
   int bpid = execpane(bottom_pane, (char*[]){"console-keyboard-basic", 0}, cfd[1]);
   close(cfd[1]);
-  int c;
-  int mfd = tym_pane_get_masterfd(top_pane);
-  while(read(cfd[0],&c,1) == 1){
-    write(mfd, &c, 1);
+
+  (void)tpid;
+  (void)bpid;
+
+  // Wait for input
+  enum {
+    PFD_SIGCHILD,
+    PFD_KEYBOARDINPUT
+  };
+
+  struct pollfd fds[] = {
+    [PFD_SIGCHILD] = {
+      .fd = sfd[0],
+      .events = POLLIN
+    },
+    [PFD_KEYBOARDINPUT] = {
+      .fd = cfd[0],
+      .events = POLLIN
+    },
+  };
+  size_t nfds = sizeof(fds)/sizeof(*fds);
+
+  while( true ){
+    int ret = poll(fds, nfds, -1);
+    if( ret == -1 ){
+      if( errno == EINTR )
+        continue;
+      perror("poll failed");
+      return 1;
+    }
+    if(!ret)
+      continue;
+    if(fds[PFD_SIGCHILD].revents & POLLIN)
+      break;
+    if(fds[PFD_KEYBOARDINPUT].revents & POLLIN){
+      int c;
+      int mfd = tym_pane_get_masterfd(top_pane);
+      while(read(cfd[0],&c,1) == 1){
+        write(mfd, &c, 1);
+      }
+    }
   }
-  while(waitpid(tpid,0,0) == -1 && errno == EINTR);
-  while(waitpid(bpid,0,0) == -1 && errno == EINTR);
-  tym_shutdown();
+
+  signal(SIGCHLD, SIG_IGN);
+  while(waitpid(-1, 0, WNOHANG) > 0); // In case a child exited before SIGCHLD was set to SIG_IGN
+
   return 0;
 }
